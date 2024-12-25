7c251214c12788b217d22ba3e02ab1ae44818289
Samuel Prasetyo <sprasetyo0511@gmail.com>
2024-12-25 10:32:50 +0700

    Fix: Logout Token Mismatch Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,5 +45,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof TokenMismatchException) {
+            // Redirect ke route logout jika token kedaluwarsa
+            return redirect('logout');
+        }
+
+        return parent::render($request, $exception);
     }
 }
