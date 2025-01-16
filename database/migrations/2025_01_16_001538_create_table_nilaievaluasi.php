7823dc17ebe1d826b5b67fc0d5d606f8fb6ec4c6
Samuel Prasetyo <sprasetyo0511@gmail.com>
2025-01-16 08:08:49 +0700

    Add new Table nilaievaluasi

---
+++ database/migrations/2025_01_16_001538_create_table_nilaievaluasi.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nilaievaluasi', function (Blueprint $table) {
+            $table->increments('idevaluasi');
+            $table->string('semester');
+            $table->string('tahunajar');
+            $table->string('algoritma');
+            $table->string('chi');
+            $table->string('dbi');
+            $table->string('ss');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nilaievaluasi');
+    }
+};
